e9146dc8601e66db8c68917ded6900b4617cde54
altyus <alctyus@gmail.com>
2014-05-12 14:50:12 -0400

    completed solution

--- Open-Me/Open-Me/FISViewController.h
+++ Open-Me/Open-Me/FISViewController.h
@@ -10,4 +10,28 @@
 
 @interface FISViewController : UIViewController
 
+
+@property (weak, nonatomic) IBOutlet UIView *bottomContainerView;
+@property (weak, nonatomic) IBOutlet UIView *topContainerView;
+
+@property (weak, nonatomic) IBOutlet UITextField *displayTextField;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *oneButton;
+@property (weak, nonatomic) IBOutlet UIButton *twoButton;
+@property (weak, nonatomic) IBOutlet UIButton *threeButton;
+@property (weak, nonatomic) IBOutlet UIButton *fourButton;
+@property (weak, nonatomic) IBOutlet UIButton *fiveButton;
+@property (weak, nonatomic) IBOutlet UIButton *sixButton;
+@property (weak, nonatomic) IBOutlet UIButton *sevenButton;
+@property (weak, nonatomic) IBOutlet UIButton *eightButton;
+@property (weak, nonatomic) IBOutlet UIButton *nineButton;
+@property (weak, nonatomic) IBOutlet UIButton *zeroButton;
+@property (weak, nonatomic) IBOutlet UIButton *decimalButton;
+@property (weak, nonatomic) IBOutlet UIButton *equalsButton;
+@property (weak, nonatomic) IBOutlet UIButton *divideButton;
+@property (weak, nonatomic) IBOutlet UIButton *multiplyButton;
+@property (weak, nonatomic) IBOutlet UIButton *minusButton;
+@property (weak, nonatomic) IBOutlet UIButton *additionButton;
+
 @end
--- Open-Me/Open-Me/FISViewController.m
+++ Open-Me/Open-Me/FISViewController.m
@@ -17,13 +17,55 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	// Do any additional setup after loading the view, typically from a nib.    
+    
+    [self.view removeConstraints:[self.view constraints]];
+    for (UIView *view in self.view.subviews)
+    {
+        [view removeConstraints:[view constraints]];
+        [view setTranslatesAutoresizingMaskIntoConstraints:NO];
+    }
+    
+    NSDictionary *views = @{@"topContainerView" : self.topContainerView,
+                            @"bottomContainerView" : self.bottomContainerView,
+                            @"displayTextField" : self.displayTextField,
+                            @"oneButton" : self.oneButton,
+                            @"twoButton" : self.twoButton,
+                            @"threeButton" : self.threeButton,
+                            @"fourButton" : self.fourButton,
+                            @"fiveButton" : self.fiveButton,
+                            @"sixButton" : self.sixButton,
+                            @"sevenButton" : self.sevenButton,
+                            @"eightButton" : self.eightButton,
+                            @"nineButton" : self.nineButton,
+                            @"zeroButton" : self.zeroButton,
+                            @"decimalButton" : self.decimalButton,
+                            @"equalsButton" : self.equalsButton,
+                            @"divideButton" : self.divideButton,
+                            @"multiplyButton" : self.multiplyButton,
+                            @"minusButton" : self.minusButton,
+                            @"additionButton" : self.additionButton};
+    
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[displayTextField]-|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(30)-[displayTextField]" options:0 metrics:nil views:views]];
+    
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[topContainerView]|" options:0 metrics:nil views:views]];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[bottomContainerView]|" options:0 metrics:nil views:views]];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[topContainerView(==bottomContainerView)][bottomContainerView(==topContainerView)]|" options:0 metrics:nil views:views]];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[oneButton(==twoButton)][twoButton(==oneButton)][threeButton(==oneButton)][divideButton(==oneButton)]|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[fourButton(==oneButton)][fiveButton(==oneButton)][sixButton(==oneButton)][multiplyButton(==oneButton)]|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[sevenButton(==oneButton)][eightButton(==oneButton)][nineButton(==oneButton)][minusButton(==oneButton)]|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[zeroButton(==oneButton)][decimalButton(==oneButton)][equalsButton(==oneButton)][additionButton(==oneButton)]|" options:0 metrics:nil views:views]];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[oneButton(==twoButton)][fourButton(==oneButton)][sevenButton(==oneButton)][zeroButton(==oneButton)]|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[twoButton(==oneButton)][fiveButton(==oneButton)][eightButton(==oneButton)][decimalButton(==oneButton)]|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[threeButton(==twoButton)][sixButton(==oneButton)][nineButton(==oneButton)][equalsButton(==oneButton)]|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[divideButton(==twoButton)][multiplyButton(==oneButton)][minusButton(==oneButton)][additionButton(==oneButton)]|" options:0 metrics:nil views:views]];
 }
 
 @end
